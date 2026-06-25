3496f7fac00f5ebc8167a208a01116480b50f61d
mozg9637-creator <mozg9637@gmail.com>
2026-06-25 22:44:48 +0300

    Delete AuraSurfApp.swift

--- AuraSurfApp.swift
+++
@@ -1,8 +0,0 @@
-// Внутри обработки кликов в AuraSurfApp.swift
-if targetLink.hasSuffix(".apkg") {
-    AuraPackageManager.shared.installApp(fromUrl: targetLink) { success in
-        if success {
-            // Показываем iOS-уведомление "Приложение установлено"
-        }
-    }
-}
